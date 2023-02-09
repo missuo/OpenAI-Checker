9b6fc2c04548912b899fe94e7148a891c0a85955
Vincent Young <missuo@pm.me>
2023-02-09 17:48:34 +0800

    feat: add print color

--- openai.sh
+++ openai.sh
@@ -3,7 +3,7 @@
  # @Author: Vincent Young
  # @Date: 2023-02-09 17:39:59
  # @LastEditors: Vincent Young
- # @LastEditTime: 2023-02-09 17:42:48
+ # @LastEditTime: 2023-02-09 17:48:00
  # @FilePath: /OpenAI-Checker/openai.sh
  # @Telegram: https://t.me/missuo
  # 
@@ -14,10 +14,12 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 PLAIN='\033[0m'
+BLUE="\033[36m"
 
 SUPPORT_COUNTRY=(AL DZ AD AO AG AR AM AU AT AZ BS BD BB BE BZ BJ BT BA BW BR BG BF CV CA CL CO KM CR HR CY DK DJ DM DO EC SV EE FJ FI FR GA GM GE DE GH GR GD GT GN GW GY HT HN HU IS IN ID IQ IE IL IT JM JP JO KZ KE KI KW KG LV LB LS LR LI LT LU MG MW MY MV ML MT MH MR MU MX MC MN ME MA MZ MM NA NR NP NL NZ NI NE NG MK NO OM PK PW PA PG PE PH PL PT QA RO RW KN LC VC WS SM ST SN RS SC SL SG SK SI SB ZA ES LK SR SE CH TH TG TO TT TN TR TV UG AE US UY VU ZM BO BN CG CZ VA FM MD PS KR TW TZ TL GB)
-echo "OpenAI Access Checker v0.0.1"
-echo 
+echo -e "${BLUE}OpenAI Access Checker. Made by Vincent${PLAIN}"
+echo -e "${BLUE}https://github.com/missuo/OpenAI-Checker${PLAIN}"
+echo "-------------------------------------"
 if [[ $(curl -sS https://chat.openai.com/ -I | grep "text/plain") != "" ]]
 then
 	echo "Your IP is BLOCKED!"
@@ -35,6 +37,7 @@ else
 			echo -e "Region: ${iso2_code4}. ${RED}Not support OpenAI at this time.${PLAIN}"
 		fi
 	fi
+	echo "-------------------------------------"
 	echo -e "[IPv6]"
 	check6=`ping6 240c::6666 -c 1 2>&1`;
 	if [[ "$check6" != *"received"* ]] && [[ "$check6" != *"transmitted"* ]];then
@@ -48,7 +51,5 @@ else
 			echo -e "Region: ${iso2_code6}. ${RED}Not support OpenAI at this time.${PLAIN}"
 		fi
 	fi
+	echo "-------------------------------------"
 fi
-echo 
-echo "Made by Vincent."
-echo "https://github.com/missuo/OpenAI-Checker"
